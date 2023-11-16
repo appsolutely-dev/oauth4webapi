c1893490ef1a84f7ef7c2368f9060df5af3a8523
Tom Hall <57662712+tom-f-hall@users.noreply.github.com>
2023-11-16 08:59:46 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -803,13 +803,13 @@ export async function discoveryRequest(
     case undefined: // Fall through
     case 'oidc':
       // MODIFIED HERE
-      if (!url.pathname.includes('openid_sandbox_configuration') {
-        if (url.pathname === '/') {
-          url.pathname = '.well-known/openid_configuration';
-        } else {
-          url.pathname = `.well-known/openid-configuration/${url.pathname}`.replace('//', '/');
-        }    
-      }
+      // if (!url.pathname.includes('openid_sandbox_configuration') {
+      //   if (url.pathname === '/') {
+      //     url.pathname = '.well-known/openid_configuration';
+      //   } else {
+      //     url.pathname = `.well-known/openid-configuration/${url.pathname}`.replace('//', '/');
+      //   }    
+      // }
       // url.pathname = `${url.pathname}/.well-known/openid-configuration`.replace('//', '/')
       break
     case 'oauth2':
